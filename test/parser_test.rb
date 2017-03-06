dc4001a119d91ce70f7ce86ce9bc553156ae86a0
David Zhang <crispgm@gmail.com>
2017-03-06 11:10:33 +0800

    Use shoulda on testing

--- test/parser_test.rb
+++ test/parser_test.rb
@@ -1,102 +1,104 @@
-require 'test_helper'
+require "test_helper"
 
 class ParserTest < Minitest::Test
-  def setup
-    @parser = Conf::Parser.new
-  end
+  context "test parser" do
+    setup do
+      @parser = Conf::Parser.new
+    end
 
-  def test_that_it_has_a_version_number
-    refute_nil ::Conf::VERSION
-  end
+    should "has a version number" do
+      refute_nil Conf::VERSION
+    end
 
-  def test_stmt_without_param
-    content = <<-EOB
-    eva;
-    EOB
+    should "parse stmt without params" do
+      content = <<-EOB
+      eva;
+      EOB
 
-    result = @parser.parse_with_debug(content)
+      result = @parser.parse_with_debug(content)
 
-    assert_equal "eva", result[0][:statement][:command]
-  end
+      assert_equal "eva", result[0][:statement][:command]
+    end
 
-  def test_stmt_with_string_param
-    content = <<-EOB
-    user  "www-data";
-    EOB
+    should "parse stmt with string param" do
+      content = <<-EOB
+      user  "www-data";
+      EOB
 
-    result = @parser.parse_with_debug(content)
+      result = @parser.parse_with_debug(content)
 
-    assert_equal "user", result[0][:statement][0][:command]
-    assert_equal "www-data", result[0][:statement][1][:string]
-  end
+      assert_equal "user", result[0][:statement][0][:command]
+      assert_equal "www-data", result[0][:statement][1][:string]
+    end
 
-  def test_stmt_with_integer_param
-    content = <<-EOB
-    worker_connections  1024;
-    EOB
-
-    result = @parser.parse_with_debug(content)
-
-    assert_equal "worker_connections", result[0][:statement][0][:command]
-    assert_equal "1024", result[0][:statement][1][:integer]
-  end
-
-  def test_stmt_with_var_param
-    content = <<-EOB
-    set  $aaa  1;
-    EOB
-
-    result = @parser.parse_with_debug(content)
-
-    assert_equal "set", result[0][:statement][0][:command]
-    assert_equal "aaa", result[0][:statement][1][:variable]
-  end
-
-  def test_stmt_with_flag_param
-    content = <<-EOB
-    flag on;
-    flag off;
-    EOB
-
-    result = @parser.parse_with_debug(content)
-
-    assert_equal "flag", result[0][:statement][0][:command]
-    assert_equal "on", result[0][:statement][1][:flag]
-    assert_equal "off", result[1][:statement][1][:flag]
-  end
-
-  def test_comment
-    content = <<-EOB
-    #error_log  logs/error.log;
-    EOB
-
-    result = @parser.parse_with_debug(content)
-
-    assert_equal "#error_log  logs/error.log;", result[0][:comment]
-  end
-
-  def test_block
-    content = <<-EOB
-    events {
+    should "parse stmt with integer param" do
+      content = <<-EOB
       worker_connections  1024;
-    }
-    EOB
+      EOB
 
-    result = @parser.parse_with_debug(content)
+      result = @parser.parse_with_debug(content)
 
-    expect = [
-      {
-        :statement => [
-          {
-            :command => "worker_connections"
-          },
-          {
-            :integer => "1024"
-          }
-        ]
+      assert_equal "worker_connections", result[0][:statement][0][:command]
+      assert_equal "1024", result[0][:statement][1][:integer]
+    end
+
+    should "parse stmt with var param" do
+      content = <<-EOB
+      set  $aaa  1;
+      EOB
+
+      result = @parser.parse_with_debug(content)
+
+      assert_equal "set", result[0][:statement][0][:command]
+      assert_equal "aaa", result[0][:statement][1][:variable]
+    end
+
+    should "parse stmt with flag param" do
+      content = <<-EOB
+      flag on;
+      flag off;
+      EOB
+
+      result = @parser.parse_with_debug(content)
+
+      assert_equal "flag", result[0][:statement][0][:command]
+      assert_equal "on", result[0][:statement][1][:flag]
+      assert_equal "off", result[1][:statement][1][:flag]
+    end
+
+    should "parse comment" do
+      content = <<-EOB
+      #error_log  logs/error.log;
+      EOB
+
+      result = @parser.parse_with_debug(content)
+
+      assert_equal "#error_log  logs/error.log;", result[0][:comment]
+    end
+
+    should "parse block" do
+      content = <<-EOB
+      events {
+        worker_connections  1024;
       }
-    ]
+      EOB
 
-    assert_equal expect, result[0][:block]
+      result = @parser.parse_with_debug(content)
+
+      expect = [
+        {
+          :statement => [
+            {
+              :command => "worker_connections"
+            },
+            {
+              :integer => "1024"
+            }
+          ]
+        }
+      ]
+
+      assert_equal expect, result[0][:block]
+    end
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,4 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
 require 'conf'
 require 'minitest/autorun'
+require 'shoulda-context'
